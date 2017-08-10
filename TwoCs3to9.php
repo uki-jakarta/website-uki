94084f6b114a74f8822af13a40ba1c493144a0bc
EvyHernyta <evy.panggabean@uki.ac.id>
2017-08-10 15:54:16 +0700

    Perbaikan tampilan

--- TwoCs3to9.php
+++ TwoCs3to9.php
@@ -11,7 +11,7 @@
         <meta name="description" content="">
         <meta name="author" content="">
 
-        <title>My Website Template</title>
+        <title>My Website Template - One Big Page</title>
 
         <link rel="icon" href="assets/img/logo.png">
         <!-- CSS -->
@@ -64,7 +64,7 @@
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#miniUKI" aria-controls="miniUKI" aria-expanded="false" aria-label="Toggle Mini View UKI">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <a class="navbar-brand" href="http://localhost:8080/TemplateBS4a6/index.php"><img src="assets/img/logo-utama.png" width="250" alt="Logo UKI"></a>
+                <a class="navbar-brand" href="#"><img src="assets/img/logo-utama.png" width="250" alt="Logo UKI"></a>
 
                 <div class="collapse navbar-collapse offset-0 offset-md-0 offset-lg-1" id="miniUKI">
                     <ul class="navbar-nav">
@@ -224,7 +224,7 @@
                                         </div>
                                         <div class="col-lg-4 col-sm-12">
                                             <a class="dropdown-item item-head disabled" href="#">Column #3</a>
-                                            <a class="dropdown-item item-head" href="OneBigPage.php">One Big Page</a>
+                                            <a class="dropdown-item item-head" href="#">Head 1</a>
                                             <a class="dropdown-item item-head" href="#">Head 2</a>
                                             <div class="dropdown-divider"></div>
                                             <a class="dropdown-item item-child" href="#">Child 1</a>
@@ -248,188 +248,29 @@
                 </div>
             </nav>
         </header>
-        <div class="wrapper">
-            <!--Begin Carousel-->
-            <div id="carouselIndicators" class="carousel-picture-slide carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselIndicators" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner" role="listbox">
-                    <div class="carousel-item active">
-                        <img class="img-fluid" src="http://placehold.it/1400x530" alt="First slide">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img-fluid" src="http://placehold.it/1400x530" alt="Second slide">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img-fluid" src="http://placehold.it/1400x530" alt="Third slide">
-                    </div>
-                </div>
-                <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
+        <div class="container wrapper">
+            <div class="page-image">
+                <img class="img-fluid" src="http://placehold.it/1400x530" alt="Page Image">
             </div>
-            <!--End Carousel-->
-            <div class="container-fluid text-carousel-wrapper">
-                <div class="subtitle">Pengumuman</div>
-                <div class="text-center p-4">
-                    <div class="quotes">when you have eliminated the impossible, whatever remains, however improbable, must be the truth</div>
-                    <div class="quoted-from">Sherlock Holmes</div>
-                </div>
-            </div>
-            <?php
-            // put your code here
-            ?>
-            <div class="container event-berita">
-                <div class="row">
-                    <div class="col-lg-8 col-md-12">
-                        <div class="berita-title">Berita Terkini</div>
-                        <div class="berita-top col-lg-12 col-md-12">
-                            <div class="card-group">
-                                <div class="card sharp-card">
-                                    <img class="card-img-top-sharp" src="http://placehold.it/200x150" alt="Card image cap">
-                                    <div class="card-block card-block-berita">
-                                        <h4 class="card-title item-berita-title">Website UKI yang Baru</h4>
-                                        <div class="text-muted item-berita-subtitle">last updated 13 Juli 2017</div>
-                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    </div>
-                                </div>
-                                <div class="card sharp-card">
-                                    <img class="card-img-top-sharp" src="http://placehold.it/200x150" alt="Card image cap">
-                                    <div class="card-block card-block-berita">
-                                        <h4 class="card-title item-berita-title">Pengembangan Website Baru</h4>
-                                        <div class="text-muted item-berita-subtitle">last updated 13 Juli 2017</div>
-                                        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                                    </div>
-                                </div>
-                                <div class="card sharp-card">
-                                    <img class="card-img-top-sharp" src="http://placehold.it/200x150" alt="Card image cap">
-                                    <div class="card-block card-block-berita">
-                                        <a href="#" class="card-title item-berita-title">PKM UKI 2017: Berjibaku di Malang Selatan </a>
-                                        <div class="text-muted item-berita-subtitle">last updated 13 Juli 2017</div>
-                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <ul class="list-unstyled">
-                            <li class="mymedia">
-                                <div class="media-body align-self-center">
-                                    <a href="#" class="item-berita-title">DR. Maruarar Siahaan, S.H.,"JANGAN SAMPAI KEHILANGAN KARAKTER AKADEMISI"</a>
-                                    <div class="text-muted item-berita-subtitle">last updated 13 Juli 2017</div>
-                                </div>
-                            </li>
-                            <div class="mymedia">
-                                <div class="media-body align-self-center">
-                                    <a href="#" class="item-berita-title">Regenerasi dalam Strategi </a>
-                                    <div class="text-muted item-berita-subtitle">last updated 6 Juli 2017</div>
-                                </div>
-                            </div>
-                        </ul>
-                        <div class="berita-end"><a href="#" class="link-next">Berita lainnya &egs;</a></div>
-                    </div>
-                    <div class="col-lg-4 col-md-12 ">
-                        <div class="event-title">Events</div>
-                        <ul class="list-unstyled">
-                            <li class="event-item">
-                                <div class="d-flex mr-3">
-                                    <div class="thumb-calendar-event">
-                                        <div class="thumb-calendar-short-date"><p class="thumb-month">Ags '17</p><p class="thumb-date">10</p></div>
-                                    </div>
-                                </div>
-                                <div class="event-body align-self-center">
-                                    <a href="#" class="item-event-title">"Menjadi yang Terbaik, Butuh Proses"</a>
-                                </div>
-                            </li>
-                            <li class="event-item">
-                                <div class="d-flex mr-3">
-                                    <div class="thumb-calendar-event">
-                                        <div class="thumb-calendar-short-date"><p class="thumb-month">Ags '17</p><p class="thumb-date">15</p></div>
-                                    </div>
-                                </div>
-                                <div class="event-body align-self-center">
-                                    <a href="#" class="item-event-title">Self-Regulated Learning FK UKI: Pdt. Esther: "Ini Bentuk Perhatian, Bukan Penghakiman."</a>
-                                </div>
-                            </li>
-                            <li class="event-item">
-                                <div class="d-flex mr-3">
-                                    <div class="thumb-calendar-event">
-                                        <div class="thumb-calendar-short-date"><p class="thumb-month">Ags '17</p><p class="thumb-date">24</p></div>
-                                    </div>
-                                </div>
-                                <div class="event-body align-self-center">
-                                    <a href="#" class="item-event-title">Program Peningkatan Kreativitas Mahasiswa</a>
-                                </div>
-                            </li>
-                            <li class="event-item">
-                                <div class="d-flex mr-3">
-                                    <div class="thumb-calendar-event">
-                                        <div class="thumb-calendar-short-date"><p class="thumb-month">Ags '17</p><p class="thumb-date">25</p></div>
-                                    </div>
-                                </div>
-                                <div class="event-body align-self-center">
-                                    <a href="#" class="item-event-title">"Mengapa Roh Kudus Penting?"</a>
-                                </div>
-                            </li>
-                            <li class="event-item">
-                                <div class="d-flex mr-3">
-                                    <div class="thumb-calendar-event">
-                                        <div class="thumb-calendar-short-date"><p class="thumb-month">Ags '17</p><p class="thumb-date">31</p></div>
-                                    </div>
-                                </div>
-                                <div class="event-body align-self-center">
-                                    <a href="#" class="item-event-title">UKI Gelar Ibadah Kebangsaan dan Doa Lintas Agama</a>
-                                </div>
-                            </li>
-                        </ul>
-                        <div class="event-end"><a href="#" class="link-next">Peristiwa lainnya &egs;</a></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="container-fluid tertiary-color">
             <div class="row">
-                <div class="card-group w-100">
-                    <div class="card w-25 subfooter accent-color">
-                        <div class="card-block">
-                            <p class="text-center">
-                                <img class="img-thumbnail" src="assets/img/logo.png" width="80" alt="UKI"/>
-                                <img class="img-thumbnail" src="http://rsuki.com/wp-content/uploads/2015/04/logobaru.jpg" width="170" alt="RS UKI"/>
-                            </p>
-                            <p class="card-text text-center"><br><br><strong>Universitas Kristen Indonesia</strong><br>Jl.Mayjen Sutoyo No 2. Jakarta<br>13630 - Indonesia</p>
-                        </div>
-                    </div>
-                    <div class="card w-25 subfooter accent-color">
-                        <div class="card-block">
-                            <ul class="list-unstyled">
-                                <li><a href="#" class="sublink-text-inverse">Fakultas Kedokteran</a></li>
-                                <li><a href="#" class="sublink-text-inverse">Fakultas Ekonomi</a></li>
-                                <li><a href="#" class="sublink-text-inverse">Fakultas Ilmu Sosial & Politik</a></li>
-                                <li><a href="#" class="sublink-text-inverse">Fakultas Sastra</a></li>
-                                <li><a href="#" class="sublink-text-inverse">Fakultas Teknik</a></li>
-                                <li><a href="#" class="sublink-text-inverse">Fakultas Hukum</a></li>
-                                <li><a href="#" class="sublink-text-inverse">Fakultas Keguruan dan Ilmu Pendidikan</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="card w-25 subfooter accent-color">
-                        <div class="card-block">
-                            <p class="card-text">This is a tweet's feed.</p>
-                        </div>
-                    </div>
-                    <div class="card w-25 subfooter accent-color">
-                        <div class="card-block">
-                            <div class="embed-responsive embed-responsive-16by9">
-                                <iframe class="embed-responsive-item" src="//www.youtube.com/embed/8iN7vUQURLA" allowfullscreen></iframe>
-                            </div>
-                        </div>
+                <div class="small-sections col-lg-3 col-md-12">
+                    Title
+                </div>
+                <div class="col-lg-9 col-md-12">
+                    <div class="page-title">This is a Title</div>
+                    <div class="page-content">
+                        <div class="page-subtitle">Lorem Ipsum</div>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,
+                        </p>
+                        <div class="page-subtitle">Cicero</div>
+                        <p>
+                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere
+                        </p>
+                        <div class="page-subtitle">Kafka</div>
+                        <p>
+                            One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. "What's happened to me?" he thought. It wasn't a dream. His room, a proper human room although a little too small, lay peacefully between its four familiar walls. A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame. It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather.
+                        </p>
                     </div>
                 </div>
             </div>
