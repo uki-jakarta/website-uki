a37eb25ce4f5089da14994f0cbc540cae0c57869
EvyHernyta <evy.panggabean@uki.ac.id>
2017-09-22 16:15:43 +0700

    minor change
    Minor Change
    +Testimonials
    +berita Slide

---
+++ application/views/v_BeritaList.php
@@ -0,0 +1,115 @@
+<script type="javascript">
+    $('#carouselBerita').carousel();
+</script>
+<div class="container wrapper">
+    <div class="row">
+        <!--Breadcrumbs-->
+        <nav class="breadcrumb col-lg-12 col-md-12">
+            <a class="breadcrumb-item" href="#">Home</a>
+            <a class="breadcrumb-item" href="#">Berita</a>
+            <a class="breadcrumb-item active" href="#">Judul Berita Slide</a>
+        </nav>
+        <!--End of Breadcrumbs-->
+        <div class="col-lg-8 col-md-12">
+            <div class="page-title">Judul Berita Slide</div>
+            <div class="text-muted berita-last-update">last updated 12 September 2017</div>
+            <div class="page-content">
+                <!--Carousel-->
+                <div id="carouselBerita" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselBerita" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselBerita" data-slide-to="1"></li>
+                        <li data-target="#carouselBerita" data-slide-to="2"></li>
+                        <li data-target="#carouselBerita" data-slide-to="3"></li>
+                    </ol>
+                    <div class="carousel-inner" role="listbox">
+                        <div class="carousel-item active">
+                            <img class="d-block img-fluid" src="http://via.placeholder.com/500x300" alt="First slide">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h3>First Slide Title</h3>
+                                <p>Lorem Ipsum</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block img-fluid" src="http://via.placeholder.com/1000x600" alt="Second slide">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h3>Second Slide Title</h3>
+                                <p>Lorem Ipsum</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block img-fluid" src="http://via.placeholder.com/500x300" alt="Fourth slide">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h3>Third Slide Title</h3>
+                                <p>Lorem Ipsum</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block img-fluid" src="http://via.placeholder.com/1500x900" alt="Third slide">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h3>Fourth Slide Title</h3>
+                                <p>Lorem Ipsum</p>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselBerita" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselBerita" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+                <!-- Carousel End-->
+                <div class="social-media-sharing">
+                    <div class="sub-u">Share via</div>
+                    <ul class="list-unstyled list-inline">
+                        <li class="list-inline-item"><a href="http://www.facebook.com/sharer.php?u=[EncodedURL]" target="_blank" rel="nofollow"><i class="fa fa-facebook-official fa-2x mod-facebook" aria-hidden="true"></i></a></li>
+                        <li class="list-inline-item"><a href="http://twitter.com/share?text=[TITLE]&url=[URL]" target="_blank" rel="nofollow"><i class="fa fa-twitter fa-2x mod-twitter" aria-hidden="true"></i></a></li>
+                        <li class="list-inline-item"><a href="mailto:?subject=[SUBJECT]&body=Check out this site [URL]" target="_blank" rel="nofollow"><i class="fa fa-envelope-o fa-2x" aria-hidden="true"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="small-sections col-lg-4 col-md-12">
+            <ul class="list-unstyled">
+                <li class="mymedia">
+                    <div class="media-body align-self-center">
+                        <a href="#" class="item-berita-title">Judul Berita Terkait #1</a>
+                        <div class="text-muted item-berita-subtitle">last updated 13 Juli 2017</div>
+                    </div>
+                    <img class="card-img img-thumbnail" src="http://placehold.it/80x80" alt="Card image">
+                </li>
+                <li class="mymedia">
+                    <div class="media-body align-self-center">
+                        <a href="#" class="item-berita-title">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget</a>
+                        <div class="text-muted item-berita-subtitle">last updated 13 Juli 2017</div>
+                    </div>
+                    <img class="card-img img-thumbnail" src="http://placehold.it/80x80" alt="Card image">
+                </li>
+                <li class="mymedia">
+                    <div class="media-body align-self-center">
+                        <a href="#" class="item-berita-title">Judul Berita Terkait #2</a>
+                        <div class="text-muted item-berita-subtitle">last updated 13 Juli 2017</div>
+                    </div>
+                    <img class="card-img img-thumbnail" src="http://placehold.it/80x80" alt="Card image">
+                </li>
+                <li class="mymedia">
+                    <div class="media-body align-self-center">
+                        <a href="#" class="item-berita-title">Judul Berita Terkait #3</a>
+                        <div class="text-muted item-berita-subtitle">last updated 13 Juli 2017</div>
+                    </div>
+                    <img class="card-img img-thumbnail" src="http://placehold.it/80x80" alt="Card image">
+                </li>
+                <li class="mymedia">
+                    <div class="media-body align-self-center">
+                        <a href="#" class="item-berita-title">Judul Berita Terkait #4</a>
+                        <div class="text-muted item-berita-subtitle">last updated 13 Juli 2017</div>
+                    </div>
+                    <img class="card-img img-thumbnail" src="http://placehold.it/80x80" alt="Card image">
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
